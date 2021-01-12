964e3c43667ad342ee1ca8b475868d2b240bdbdb
Gosia <malgbogd@gmail.com>
2021-01-12 16:44:23 +0100

    CSS finished, Script started, constructor done, without displaying function yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,63 @@
-let myLibrary = [];
+let myBooks= [];
+const myLibrary = document.querySelector('.bookMenu')
+let title;
+let author;
+let pages;
+let read;
 
-function Book() {
-  // the constructor...
+
+
+function Book(title,author,pages,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  getInput()
+  checkIfEmpty();
+const book = new Book(title,author,pages,read);
+console.log(book);
 }
+
+function getInput() {
+title = document.getElementById("title").value;
+author = document.getElementById("author").value;
+pages = document.getElementById("pages").value;
+read = document.getElementById("read").value;
+return title, author, pages, read;
+}
+
+function checkIfEmpty(){
+  checkTitle();
+  checkAuthor();
+  checkPages();
+  checkRead();
+}
+
+function checkTitle() {
+  if (title.length == 0) {
+    alert("Please add the title.");
+  }
+}
+
+function checkAuthor() {
+  if (author.length == 0) {
+    alert("Please add the author.");
+  }
+}
+
+function checkPages() {
+  if ((pages.length == 0) || (isNaN(Number(pages))) || (Number(pages)<=0)) {
+    alert("How many pages is the book?");
+  }
+}
+
+function checkRead() {
+  if (read.length == 0) {
+    alert("Did you read the book?");
+  } 
+}
+
+document.getElementById('add').addEventListener('click', addBookToLibrary);
